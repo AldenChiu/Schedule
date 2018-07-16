f5d8cbbcfc50deffa05741cf1a733bd1dee44aa4
student <aldenchiu13@gmail.com>
2018-07-16 10:39:39 -0600

    Added ability for description to show when button is pressed

--- src/components/arrow.js
+++ src/components/arrow.js
@@ -11,18 +11,20 @@ class Arrow extends Component {
     }
 
     toggleArrow = function() {
+        this.props.callback(this.state.status)
         if(this.state.status) {
-            document.getElementById('arrow').classList.remove('arrow-closed');
+            document.getElementById(this.id).classList.remove('arrow-closed');
         } else {
-            document.getElementById('arrow').classList.add('arrow-closed');
+            document.getElementById(this.id).classList.add('arrow-closed');
         }
 
         this.setState({ status: !this.state.status })
     }.bind(this);
     
     render() {
+        this.id  = `arrow-${this.props.id}`
         return (
-            <a onClick={() => this.toggleArrow()} className={`${this.props.className} arrow`}/>
+            <a id={this.id} onClick = {() => this.toggleArrow()}className={`${this.props.className} arrow`}/>
         )
     }
 }
--- src/components/library/libraryCourse.js
+++ src/components/library/libraryCourse.js
@@ -7,22 +7,48 @@ import Arrow from '../arrow';
 import Action from '../action';
 
 class LibraryCourse extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            status: true
+        }
+    }
+
+    renderDescription = function() {
+        if(!this.state.status) {
+            return (
+            <div className="library-course__description">
+            <label>Course Description</label>
+                <p>
+                { this.props.description }
+                </p>
+            </div>
+          )
+        }
+    }.bind(this)
+
+    handleCallback = function(status) {
+        if(!status) {
+            document.getElementById('library-course').classList.add('library-course-selected');
+        } else {
+            document.getElementById('library-course').classList.remove('library-course-selected');
+        }
+        this.setState({ status })
+    }.bind(this)
+
     render() {
         return (
-            <div className="library-course">
+            <div id="library-course" className="library-course">
                 <div className="library-course__title-check">
                     <label className="library-course__title">{ this.props.title }</label>
                     { Icon('fas fa-check', 'library-course__icon') }
                 </div>
                 <div className="library-course__line"></div>
-                <Arrow className="library-course__arrow"/>
+                <Arrow callback={(status) => this.handleCallback(status)} id={this.props.id} className="library-course__arrow"/>
                 <Action onClick={() => this.props.toggleEnrolled(this.props.id)} className="library-course__action"/>
-                <div className="library-course__description">
-                    <label>Course Description</label>
-                    <p>
-                        { this.props.description }
-                    </p>
-                </div>
+                { this.renderDescription() }
             </div>
         )
     }
