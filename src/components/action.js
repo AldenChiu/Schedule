fdca4ab51f6efd3bb1b2c6af043c02e2d235d8e3
student <aldenchiu13@gmail.com>
2018-07-16 12:00:47 -0600

    Buttons work really well

--- src/components/action.js
+++ src/components/action.js
@@ -2,12 +2,42 @@ import React, { Component } from 'react';
 import Icon from './icon';
 
 class Action extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.status = false;
+    }
+
+    handleAction = function() {
+        this.props.onClick()
+
+        if(!this.status) {
+            document.getElementById(this.id).classList.add('action-remove');
+        } else {
+            document.getElementById(this.id).classList.remove('action-remove');
+        }
+        this.status = !this.status;
+
+    }.bind(this);
+
     render() {
+        this.id = `action ${this.props.id}`
         return (
-            <button onClick={() => this.props.onClick()} className={`${this.props.className} action`}>
-                { Icon('fas fa-plus-circle') }
-            </button>
+            <a 
+                id='action'
+                id = {this.id}
+                onClick={() => this.handleAction()}
+                className={`${this.props.className} action`}>
+
+            </a>
         )
+        
+        // return (
+        //     <button onClick={() => this.props.onClick()} className={`${this.props.className} action`}>
+        //         { Icon('fas fa-plus-circle') }
+        //     </button>
+        // )
     }
 }
 
--- src/components/library/libraryCourse.js
+++ src/components/library/libraryCourse.js
@@ -6,49 +6,49 @@ import Icon from '../icon';
 import Arrow from '../arrow';
 import Action from '../action';
 
+import AnimateHeight from 'react-animate-height';
+
 class LibraryCourse extends Component {
 
     constructor(props) {
         super(props)
 
         this.state = {
-            status: true
+            status: true,
+            height: 0
         }
     }
 
-    renderDescription = function() {
-        if(!this.state.status) {
-            return (
-            <div className="library-course__description">
-            <label>Course Description</label>
-                <p>
-                { this.props.description }
-                </p>
-            </div>
-          )
-        }
-    }.bind(this)
+
 
     handleCallback = function(status) {
+        let height = this.state.height == 0 ? 'auto' : 0;
         if(!status) {
-            document.getElementById('library-course').classList.add('library-course-selected');
+            document.getElementById(this.id).classList.add('library-course-selected');
         } else {
-            document.getElementById('library-course').classList.remove('library-course-selected');
+            document.getElementById(this.id).classList.remove('library-course-selected');
         }
-        this.setState({ status })
+        this.setState({ status, height })
     }.bind(this)
 
     render() {
+        this.id = `library-course-${this.props.id}`
         return (
-            <div id="library-course" className="library-course">
+            <div id={this.id} className="library-course">
                 <div className="library-course__title-check">
-                    <label className="library-course__title">{ this.props.title }</label>
+                    <div className="library-course__title">{ this.props.title }</div>
                     { Icon('fas fa-check', 'library-course__icon') }
                 </div>
-                <div className="library-course__line"></div>
                 <Arrow callback={(status) => this.handleCallback(status)} id={this.props.id} className="library-course__arrow"/>
-                <Action onClick={() => this.props.toggleEnrolled(this.props.id)} className="library-course__action"/>
-                { this.renderDescription() }
+                <Action id = {this.props.id} onClick={() => this.props.toggleEnrolled(this.props.id)} className="library-course__action"/>
+                <AnimateHeight duration = {300} height={this.state.height}>
+                <div className="library-course__description">
+                  <label>Course Description</label>
+                  <p>
+                  { this.props.description }
+                  </p>
+                </div>
+                </AnimateHeight>
             </div>
         )
     }
